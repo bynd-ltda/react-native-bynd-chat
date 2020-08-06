8aa96e945e9ef2266db04f6533ec9469b8a90e5b
Abraao barros lacerda <abraaobarros3@gmail.com>
2020-08-06 18:10:23 -0300

    Add layout for other users

--- src/components/ChatMessage/text.tsx
+++ src/components/ChatMessage/text.tsx
@@ -1,16 +1,72 @@
 import React from 'react';
 import {parseMessage} from '../../helpers';
-import {Text, StyleSheet} from 'react-native';
+import {Text, StyleSheet, View} from 'react-native';
 import {IChatMessageProps} from '.';
 import { BackgroundColor } from '../../chat/styles';
+import { getUser } from '../../services/requests';
 
 export const ChatTextMessage: React.FC<IChatMessageProps> = (props) => {
-  return <Text style={styles.text}> {parseMessage(props.text)}</Text>;
+  const isMe = () => {
+    return getUser().id === props?.sender?.id
+
+  }
+  const styles = isMe() ? stylesPurple: stylesBlack
+  return <View style={styles.textBubble}>
+            <View style={styles.textContainer}>
+              <Text style={styles.text}> {parseMessage(props.text)}</Text>
+            </View>
+          </View>
 };
 
-const styles = StyleSheet.create({
+
+const stylesPurple = StyleSheet.create({
     text: {
+      fontSize: 16,
+      color:'white',
+
+    }, 
+    textBubble:{
+      flexDirection: 'row',
+
+    },
+    timeBubble: {
       color: 'white',
-      fontSize: 16
-    }
+      fontSize: 11
+    },
+    textContainer:{
+      justifyContent:'flex-end',
+      flexShrink:1,
+      margin: 12,
+      marginBottom:8 
+    },
+    userNameStyle: {
+      fontWeight: 'bold',
+      textAlign: 'right'
+    },
+})
+
+const stylesBlack = StyleSheet.create({
+  text: {
+    fontSize: 16,
+    color:'black',
+    
+  },
+  textBubble:{
+    flexDirection: 'row'
+  },
+  timeBubble: {
+    color: 'white',
+    fontSize: 11
+  },
+  textContainer:{
+
+    justifyContent:'flex-start',
+    flexShrink:1,
+    margin: 12,
+    marginBottom:8 
+  },
+  userNameStyle: {
+    fontWeight: 'bold',
+    textAlign: 'left'
+  },
 })
--- src/components/ChatMessage/wrapper.tsx
+++ src/components/ChatMessage/wrapper.tsx
@@ -3,10 +3,18 @@ import {IChatMessageProps} from '.';
 import {isSameHourAndMinute, parseMessage} from '../../helpers';
 import {View, Text} from 'react-native';
 import moment from 'moment';
+import { getUser, getBChatSetup } from '../../services/requests';
+import { useRoute } from '@react-navigation/native';
 
 export const ChatMessageGroup: React.FC<IChatMessageProps> = (props) => {
+  
+  const isMe = () => {
+    return getUser().id === props?.sender?.id
+
+  }
+
   const hasToShowTime = () => {
-    return true
+
     return (
       !props.next ||
       (props.next.sender_id === props.sender_id &&
@@ -14,7 +22,12 @@ export const ChatMessageGroup: React.FC<IChatMessageProps> = (props) => {
     );
   };
 
+
+
   const hasToShowName = (): boolean => {
+    if(isMe()){
+      return false
+    }
     if (props.previous === null) {
       return true;
     }
@@ -30,6 +43,8 @@ export const ChatMessageGroup: React.FC<IChatMessageProps> = (props) => {
     return true;
   };
 
+  const styles = isMe() ? stylesPurple: stylesGray
+
   return (
     <View>
     {hasToShowName() && (
@@ -37,23 +52,20 @@ export const ChatMessageGroup: React.FC<IChatMessageProps> = (props) => {
       )}
     <View style={styles.containerMessageStyle}>
       <View style={styles.textWrapper}>
-        <View style={styles.textBubble}>
-          <View style={styles.textContainer}>
-            {props.children}
-          </View>
-        </View>
-        {hasToShowTime() && (
+          {props.children}
+      
+
          <Text style={styles.timeBubble}>{parseMessage(moment(props.created_at).format('HH:mm'))}</Text>
-        )}
+
+        </View>
       </View>
-    </View>
     </View>
   );
 };
 
 
 
-const styles = {
+const stylesPurple = {
   containerMessageStyle: {
     flexDirection: 'row',
     justifyContent: 'flex-end'
@@ -70,23 +82,52 @@ const styles = {
     marginBottom:4, 
     minWidth: 100
   },
-  textBubble:{
-    flexDirection: 'row'
-    
-  },
+
   timeBubble: {
     color: 'white',
     fontSize: 11
-  },
-  textContainer:{
-    flexWrap: 'wrap',
-    justifyContent:'flex-end',
-    flexShrink:1,
-    margin: 12,
-    marginBottom:8 
   },
   userNameStyle: {
     fontWeight: 'bold',
     textAlign: 'right'
   },
 };
+
+
+const stylesGray = {
+  containerMessageStyle: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start'
+  },
+  textWrapper:{
+    flexDirection:'column',
+    alignItems:'flex-start',
+    padding: 8,
+    backgroundColor:'#D3D3D3',
+    borderTopLeftRadius:30,
+    borderTopRightRadius:30,
+    borderBottomRightRadius:30,
+    marginTop:4,
+    marginBottom:4, 
+    minWidth: 100
+  },
+  textBubble:{
+    color:'red'
+  },
+  timeBubble: {
+    color: '#8E8E8E',
+    fontSize: 11,
+    marginLeft: 12,
+  },
+  textContainer:{
+    flexWrap: 'wrap',
+    justifyContent:'flex-start',
+    flexShrink:1,
+    margin: 12,
+    marginBottom:8 
+  },
+  userNameStyle: {
+    fontWeight: 'bold',
+    textAlign: 'left'
+  },
+}
