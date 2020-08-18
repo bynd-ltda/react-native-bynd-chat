caecc47a301bb9ba2c21c5e04583723b9fa51de7
Icaro Santos <Icarooliveira_23@hotmail.com>
2020-08-18 10:13:08 -0300

    keyboardAvoidView Android

--- src/components/ChatDetail/index.tsx
+++ src/components/ChatDetail/index.tsx
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react';
-import {useMessages, useLoading} from '../../hooks';
-import {IChat, IMessage} from '../../services/models';
-import {useRoute} from '@react-navigation/native';
+import { useMessages, useLoading } from '../../hooks';
+import { IChat, IMessage } from '../../services/models';
+import { useRoute } from '@react-navigation/native';
 
 import {
   Text,
@@ -14,11 +14,11 @@ import {
   ScrollView,
 } from 'react-native';
 import ChatMessage from '../ChatMessage';
-import {sectionsFrom} from '../../helpers';
+import { sectionsFrom } from '../../helpers';
 import moment from 'moment';
 import ChatMessageInput from '../ChatMessageInput';
 
-interface IChatDetailProps {}
+interface IChatDetailProps { }
 
 const ChatDetailComponent: React.FC<IChatDetailProps> = (props) => {
   const route = useRoute();
@@ -41,7 +41,7 @@ const ChatDetailComponent: React.FC<IChatDetailProps> = (props) => {
   const sectionsList = useRef<SectionList>()
 
 
-  const submit = (message:IMessage) => {
+  const submit = (message: IMessage) => {
     send(message)
     sectionsList.current.scrollToLocation({
       animated: true,
@@ -51,41 +51,48 @@ const ChatDetailComponent: React.FC<IChatDetailProps> = (props) => {
     })
   }
   return (
-      <KeyboardAvoidingView behavior='padding' style={{flex:1}}>
-          <SectionList<IMessage>
-            ref={ref => sectionsList.current = ref}
-            sections={sections}
-            style={{ flex:1, padding:16}}
-            stickySectionHeadersEnabled
-            inverted
-            renderSectionFooter={({section: {title}}) => <SectionComponent>{title}</SectionComponent>}
-            keyExtractor={(item, key) => `${item.id} + ${key}`}
-            renderItem={({item, section, index}) => (
-              <ChatMessage
-                {...item}
-                sender={users[item.sender_id]}
-                next={
-                  index < section.data.length ? section.data[index + 1] : null
-                }
-                previous={index > 0 ? section.data[index - 1] : null}
-              />
-            )}
-            refreshing={pending}
-            onRefresh={reload}
+    <KeyboardAvoidingView
+      keyboardVerticalOffset={Platform.select({
+        //ios: isIphoneX() ? 85 : 65,
+        android: 25,
+      })}
+      behavior={Platform.select({ ios: 'padding', android: null })}
+      style={{ flex: 1 }}>
+
+      <SectionList<IMessage>
+        ref={ref => sectionsList.current = ref}
+        sections={sections}
+        style={{ flex: 1, padding: 16 }}
+        stickySectionHeadersEnabled
+        inverted
+        renderSectionFooter={({ section: { title } }) => <SectionComponent>{title}</SectionComponent>}
+        keyExtractor={(item, key) => `${item.id} + ${key}`}
+        renderItem={({ item, section, index }) => (
+          <ChatMessage
+            {...item}
+            sender={users[item.sender_id]}
+            next={
+              index < section.data.length ? section.data[index + 1] : null
+            }
+            previous={index > 0 ? section.data[index - 1] : null}
           />
-        <ChatMessageInput onSubmit={submit} />
-      </KeyboardAvoidingView>
+        )}
+        refreshing={pending}
+        onRefresh={reload}
+      />
+      <ChatMessageInput onSubmit={submit} />
+    </KeyboardAvoidingView >
   );
 };
 
-const SectionComponent = ({children}) => {
-  return <Text style={{textAlign:'center', color:'#8E8E8E'}}>{moment(children).isValid() && moment(children).format('LL')}</Text>
+const SectionComponent = ({ children }) => {
+  return <Text style={{ textAlign: 'center', color: '#8E8E8E' }}>{moment(children).isValid() && moment(children).format('LL')}</Text>
 }
 
 const styles = StyleSheet.create({
   messageListStyle: {
     flexDirection: 'column',
-    flex:1
+    flex: 1
   },
 });
 
